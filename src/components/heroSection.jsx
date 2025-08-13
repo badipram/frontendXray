42b1a2c4e4a119393728624901637644fa494c3c
badi amnu <badipram23@outlook.com>
2025-08-13 13:15:59 +0700

    fixing button

--- src/components/heroSection.jsx
+++ src/components/heroSection.jsx
@@ -4,7 +4,7 @@ const HeroSection = () => {
             <div className="hero-text">
                 <h1>Rekonstruksi dan Deteksi Citra <span className="blue">X-ray </span> Berbasis <span className="red">AI</span></h1>
                 <h3>Sistem cerdas berbasis Computer Vision untuk membantu identifikasi dan analisis citra X-ray secara akurat dan efisien. Mendukung diagnosa medis dengan visualisasi yang lebih jelas dan tersrtuktur</h3>
-                <a href="#learn-more" className="btn">Unggah Citra Xray</a>
+                <a href="#rekonstruksi" className="btn">Unggah Citra Xray</a>
             </div>
             <div className="hero-image">
                 <img src="/CT scan-pana.png" alt="ilustration" />
